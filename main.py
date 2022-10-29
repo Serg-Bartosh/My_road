7da7af3caee17400ba5c3dd01ce978bb7d5818cf
Onehayzent <sergbartosh@gmail.com>
2022-10-29 15:42:25 +0300

    was added get_number_triangle_3
    link --> https://stepik.org/lesson/298796/step/1?unit=280623

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-def numerical_triangle(num):
-    for i in range(1,num+1,1):
-        print(f'{i}'*i, end='\n')
+def get_number_triangle_3(num):
+    total = 0
+    for i in range(1, num+1, 1):
+        for j in range(1, i+1):
+            total += 1
+            print(f'{total}', end=' ')
+        print()
 
 
 num = int(input())
+get_number_triangle_3(num)
 
-numerical_triangle(num)
-
-#Дано натуральное число nn. Напишите программу,
-# которая печатает численный треугольник в соответствии с примером:
+#Дано натуральное число nn.
+# Напишите программу, которая печатает численный треугольник с высотой равной nn,
+# в соответствии с примером:
 # 1
-# 22
-# 333
-# 4444
-# 55555
+# 2 3
+# 4 5 6
+# 7 8 9 10
+# 11 12 13 14 15
+# 16 17 18 19 20 21
+# ...
