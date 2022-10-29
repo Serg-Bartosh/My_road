031f22a5a857afba612a4fdba74a1fe47367925a
Onehayzent <sergbartosh@gmail.com>
2022-10-29 17:17:55 +0300

    was added dividers2
    link --> https://stepik.org/lesson/298796/step/4?unit=280623

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-def dividers(border_1, border_2):
-    summ_count = 0
-    max_i = 0
-    for i in range(border_1, border_2 + 1):
+def dividers2(border):
+    for i in range(1, border+1, 1):
         count = 0
-        for j in range(1, i + 1):
+        for j in range(1, i+1):
             if i % j == 0:
-                count += j
-            if count >= summ_count:
-                summ_count = count
-                max_i = i
-    print(max_i, summ_count)
+                count += 1
+        print(f'{i}{count * "+"}')
 
 
-border_1, border_2 = map(int, input().split())
-dividers(border_1, border_2)
 
-#На вход программе подается два натуральных числа aa и bb (a < ba< b). Напишите программу,
-# которая находит натуральное число из отрезка [a; \, b][a;b] с максимальной суммой делителей.
+border = int(input())
+dividers2(border)
+
+#На вход программе подается натуральное число nn. Напишите программу,
+# выводящую графическое изображение делимости чисел от 11 до nn включительно.
+# В каждой строке надо напечатать очередное число и столько символов «+», сколько делителей у этого числа.
