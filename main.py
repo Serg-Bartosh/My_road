a706338578758aeb8fbf2103716a74e2475c957e
Onehayzent <sergbartosh@gmail.com>
2022-10-29 14:28:26 +0300

    was added numerical_triangle
    link --> https://stepik.org/lesson/298795/step/9?unit=280622

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-def star_triangle(num):
-    for i in range(1, num // 2 + 2):
-        print('*' * i, end='')
-        print()
-    for i in range(num // 2, 0, -1):
-        print('*' * i, end='')
-        print()
+def numerical_triangle(num):
+    for i in range(1,num+1,1):
+        print(f'{i}'*i, end='\n')
 
 
 num = int(input())
 
-star_triangle(num)
+numerical_triangle(num)
 
-#Звездный треугольник .
-# Дано нечетное натуральное число nn. Напишите программу,
-# которая печатает равнобедренный звездный треугольник с основанием,
-# равным nn в соответствии с примером:
+#Дано натуральное число nn. Напишите программу,
+# которая печатает численный треугольник в соответствии с примером:
+# 1
+# 22
+# 333
+# 4444
+# 55555
