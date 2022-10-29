d458fccdf4ba8dc0a471d4d0d31ec7dec408c5dd
Onehayzent <sergbartosh@gmail.com>
2022-10-29 13:43:36 +0300

    was added star_triangle
    link --> https://stepik.org/lesson/298795/step/8?thread=solutions&unit=280622

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-def tabl(num):
-    for i in range(1, num+1, 1):
-        for j in range(1, 10):
-            print(f'{i} + {j} = {i+j}')
+def star_triangle(num):
+    for i in range(1, num // 2 + 2):
+        print('*' * i, end='')
+        print()
+    for i in range(num // 2, 0, -1):
+        print('*' * i, end='')
         print()
 
 
 num = int(input())
 
-tabl(num)
+star_triangle(num)
+
+#Звездный треугольник .
+# Дано нечетное натуральное число nn. Напишите программу,
+# которая печатает равнобедренный звездный треугольник с основанием,
+# равным nn в соответствии с примером:
